c7cf4862e174a1c53dc85908fd5d0d3645edb18f
Surya <s.surya2727@gmail.com>
2024-03-12 14:00:59 +0530

    Login page updated

--- src/components/User/Login.jsx
+++ src/components/User/Login.jsx
@@ -64,7 +64,7 @@ const LoginForm = () => {
         <div className="col-md-6 offset-md-3 col-xl-4 offset-xl-4">
           <Card className="shadow">
             <img
-              src="https://images.unsplash.com/photo-1600880292203-757bb62b4baf?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              src="https://www.go.ooo/img/bg-img/Login.jpg"
               alt=""
               className="card-img-top"
             />
